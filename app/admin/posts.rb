ff8c56952b1e04199b1b5a9156c490631286f2ac
Njonge9 <frednjonge9@gmail.com>
2023-10-14 18:13:31 +0300

    Add a button for publishing an article

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -15,4 +15,14 @@ ActiveAdmin.register Post do
   #   permitted
   # end
 
+  action_item :publish, only: :show do
+    link_to 'Publish', publish_admin_post_path(post), method: :put if !post.published_at?
+  end
+
+  member_action :publish, method: :put do
+    post = Post.find(params[:id])
+    post.update(published_at: Time.zone.now)
+    redirect_to admin_post_path(post)
+  end
+
 end
